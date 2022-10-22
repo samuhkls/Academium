b8704d09e49d30fa84bad669f2e41b7a9c6b01b9
samuhkls <samuel.henriquess21@gmail.com>
2022-10-22 20:15:19 -0300

    começo script do registrar

---
+++ Site/Scripts/registrarPopup.js
@@ -0,0 +1,6 @@
+function abreRegistrar(){
+    document.getElementById("popup").style.display = "none"
+}
+function fechaRegistrar(){
+    document.getElementById("popup").style.display = "none";
+}
