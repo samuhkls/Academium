d6b14172f35c353221f873c536a236951f48d913
samuhkls <samuel.henriquess21@gmail.com>
2022-11-06 01:03:22 -0300

    falha

--- Site/CursosPage.php
+++ Site/CursosPage.php
@@ -3,7 +3,7 @@
     include_once('./ConexaoBd/conex.php');
 
     $sql = "SELECT * FROM alura_cursos ORDER BY id_curso DESC";
-    
+
     $result = $conexao->query($sql);
 
 ?>
@@ -18,7 +18,7 @@
     <link rel="shortcut icon" href=".\Src\icon Academium.ico">
     <script src = "./Scripts/SearchBar.js" defer></script>
 </head>
-<body>    
+<body>
     <div id="container">
 
         <div class="Content">
@@ -34,34 +34,33 @@
                 </div>
             </div>
         </div>
-         
+
          <div class = "btnContainer">
              <button id="botoes" onclick= "sortCarga()">Carga ↕</button>
              <button id="botoes" onclick = "sortAvaliacao()">Avaliação ↕</button>
              <button id="botoes">Instrutor ↕</button>
         </div>
-            
+
         <div class="resultados_cursos" id="resultados_cursos">
             <?php
                 while($user_data = mysqli_fetch_assoc($result)){
-                    echo "<div class='div_curso'>";
+                    echo "<div class = 'div_curso' id='div_curso'>";
                         echo "<a href='".$user_data['link_curso']."' target='_blank'>";
-                        echo "<p class='curso' id= 'curso_nome'>".$user_data['id_curso']."</p>";
                         echo "<p class='curso_nome' id= 'curso_nome'>".$user_data['curso_nome']."</p>";
-                        echo "<p class='curso' id= 'curso_horas'>".$user_data['carga_horaria']."h</p>";
+                        echo "<p class='curso_carga' id= 'curso_horas'>".$user_data['carga_horaria']."h</p>";
                         echo "<p class='curso' id= 'curso_preco'>R$ ".$user_data['preco_curso']."</p>";
-                        echo "<p class= 'curso_avaliacao'>".$user_data['avaliacao_curso']."</p>";
+                        echo "<p class='curso_avaliacao' id= 'curso_avaliacao'>".$user_data['avaliacao_curso']."</p>";
                         echo "<p class='curso' id= 'curso_topico'>".$user_data['topico_curso']."</p>";
                         echo "<p class='curso' id= 'curso_professor '>".$user_data['instrutor']."</p>";
                         echo "</a>";
                     echo "</div>";
                 }
             ?>
-        </div>        
+        </div>
     </div>
 </div>
-    
-<script src = "https://code.jquery.com/jquery-1.9.1.js"></script>
+
+
 <script src = "./Scripts/sortCursos.js"> </script>
 
 
--- Site/Scripts/sortCursos.js
+++ Site/Scripts/sortCursos.js
@@ -4,14 +4,13 @@ function sortAvaliacao() {
   console.log(cursos);
   troca = true;
   // loop da troca
-  while (troca) {
-    troca = false;
+
     b = document.getElementsByClassName('curso_avaliacao');
     b = Array.from(b);
     //console.log(b)
     // loop por todos os elementos:
     for (i = 0; i < (b.length ); i++) {
-      console.log(b);
+
       checkTroca = false;
       // check para ver se o proximo item sera trocado
       if (Number(b[i].innerHTML) < Number(b[i + 1].innerHTML)) {
@@ -21,27 +20,28 @@ function sortAvaliacao() {
       }
     }
     if (checkTroca) {
-      // se a troca foi feita, marca troca como true para refazer o loop
+      // se a checkTroca for marcado como true, troca no html e altera o valor de troca para o loop
       cursos[i].parentNode.insertBefore(cursos[i + 1], cursos[i]);
       troca = true;
     }
   }
-}
+
 
 
 
   
 function sortCarga() {
   var i, troca, b, checkTroca;
-  cursos = document.getElementsByClassName("curso");
-  console.log(b);
+  cursos = document.getElementsByClassName("div_curso");
+  console.log(cursos);
   troca = true;
   // loop da troca
-  while (troca) {
-    troca = false;
-    b = document.getElementsByClassName('carga');
+
+    b = document.getElementsByClassName('curso_carga');
+    b = Array.from(b);
+    //console.log(b)
     // loop por todos os elementos:
-    for (i = 0; i < (b.length - 1); i++) {
+    for (i = 0; i < (b.length ); i++) {
 
       checkTroca = false;
       // check para ver se o proximo item sera trocado
@@ -52,10 +52,9 @@ function sortCarga() {
       }
     }
     if (checkTroca) {
-      // se a troca foi feita, marca troca como true para refazer o loop
-      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      // se a checkTroca for marcado como true, troca no html e altera o valor de troca para o loop
+      cursos[i].parentNode.insertBefore(cursos[i + 1], cursos[i]);
       troca = true;
     }
-  }
 }
 
